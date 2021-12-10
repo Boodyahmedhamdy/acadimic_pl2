efc93264b3763ed928ff55fe4d0b64274a15cc59
unknown <abdelrahmanahmedhamdy99@gmail.com>
2021-12-10 13:39:49 +0200

    modified to be commited in word file

--- projects/for_sheet_3/src/Main.java
+++ projects/for_sheet_3/src/Main.java
@@ -11,3 +11,72 @@ public class Main {
         System.out.println("the account was created in " + account.getDateCreated());
     }
 }
+
+/*
+class Account {
+    private int ID;
+    private double balance;
+    private double annualInterestRate;
+    private LocalDate dateCreated;
+
+    // the default constructor
+    Account() {
+        this.ID = 0;
+        this.balance = 0;
+        this.annualInterestRate = 0;
+        this.dateCreated = LocalDate.now();
+    }
+
+    // another constructor
+    Account(int id, double balance) {
+        this.ID = id;
+        this.balance = balance;
+        this.annualInterestRate = 0;
+        this.dateCreated = LocalDate.now();
+    }
+
+    // getters and setters
+
+    // ID
+    public int getID() {
+        return this.ID;
+    }
+    public void setID(int id){
+        this.ID = id;
+    }
+
+    // balance
+    public double getBalance() {
+        return this.balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // annual interest rate
+    public double getAnnualInterestRate() {
+        return this.annualInterestRate;
+    }
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    // date created
+    public LocalDate getDateCreated() {
+        return this.dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return ((this.annualInterestRate / 100) * this.balance) / 12.0;
+    }
+
+    public void withdraw(double money) {
+        this.balance = this.balance - money;
+    }
+
+    public void depoist(double money) {
+        this.balance = this.balance + money;
+    }
+
+}
+*/
